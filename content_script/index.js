a7ae900e3e48aeb5237662f18a176e2617ab7c83
Samuel Chan <cchin.samuel@gmail.com>
2019-12-09 10:03:36 +0800

    style: use bigger icon

--- content_script/index.js
+++ content_script/index.js
@@ -1,11 +1,11 @@
 async function onClickNavigator(event) {
   console.log('Send to background')
-  browser.runtime.sendMessage("Do something");
+  browser.runtime.sendMessage("Do something")
 }
 
 let image = document.createElement('img')
 image.setAttribute('src', 'https://icon-library.net/images/icon-circle-png/icon-circle-png-29.jpg')
-image.style.cssText = 'width: 15px; height: 15px; opacity: 0.3'
+image.style.cssText = 'width: 30px; height: 30px; opacity: 0.3'
 image.setAttribute('draggable', 'true')
 
 let navigationDiv = document.createElement('div')
